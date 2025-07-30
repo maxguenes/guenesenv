27d109e8d3a4fa2fc652f3876af062a1dbd6908c
Max Guenes <maxguenes@gmail.com>
2025-07-29 23:14:01 -0300

    Ajusta merge usado

--- scripts/installers/git_tools.sh
+++ scripts/installers/git_tools.sh
@@ -10,7 +10,7 @@ rm "git-delta_0.18.2_amd64.deb"
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
-git config --global merge.conflictStyle zdiff3
+git config --global merge.conflictStyle zdiff
 
 if ! which gitkraken >/dev/null; then
   wget "https://api.gitkraken.dev/releases/production/linux/x64/active/gitkraken-amd64.deb"
