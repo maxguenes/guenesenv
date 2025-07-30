4df91580591927351e6b062acde5b63e8fa62ccc
Max Guenes <maxguenes@gmail.com>
2025-07-29 22:32:26 -0300

    Adiciona ghostty e lazygit

--- scripts/installers/neovim.sh
+++ scripts/installers/neovim.sh
@@ -17,5 +17,20 @@ if [ ! -e "~/Development/neovim/bin/nvim" ]; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
   tar -C ~/Development/neovim -xzf nvim-linux-x86_64.tar.gz
   rm nvim-linux-x86_64.tar.gz
+
+  popd >/dev/null
+fi
+
+if which lazygit >/dev/null; then
+  pushd ~/Downloads/ >/dev/null
+
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+
+  rm lazygit
+  rm lazygit.tar.gz
+
   popd >/dev/null
 fi
