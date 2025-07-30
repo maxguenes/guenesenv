fd0e3cfc93b34c5638150b177082ce74c2cac6b1
Max Guenes <max.santos@e-deploy.com.br>
2025-07-30 09:33:45 -0300

    Ajusta delete de arquivos e git config

--- scripts/installers/ghostty.sh
+++ scripts/installers/ghostty.sh
@@ -8,5 +8,5 @@ BASEDIR=$(dirname "$0")
 if ! which ghostty >/dev/null; then
   wget "https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.1.3-0-ppa2/ghostty_1.1.3-0.ppa2_amd64_25.04.deb"
   dpkg -i "ghostty_1.1.3-0.ppa2_amd64_25.04.deb"
-  rm "ghostty_1.1.3-0.ppa2_amd64_25.04.deb"
+  rm "ghostty_1.1.3-0.ppa2_amd64_25.04.deb*"
 fi
--- scripts/installers/git_tools.sh
+++ scripts/installers/git_tools.sh
@@ -7,12 +7,12 @@ BASEDIR=$(dirname "$0")
 
 wget "https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb"
 dpkg -i "git-delta_0.18.2_amd64.deb"
-rm "git-delta_0.18.2_amd64.deb"
+rm "git-delta_0.18.2_amd64.deb*"
 
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
-git config --global merge.conflictStyle zdiff
+git config --global merge.conflictStyle zdiff3
 
 if ! which gitkraken >/dev/null; then
   wget "https://api.gitkraken.dev/releases/production/linux/x64/active/gitkraken-amd64.deb"
