79a2d5feacc27eeccdd909a0508735177cd2546e
Max Guenes <maxguenes@gmail.com>
2025-07-31 10:20:47 -0300

    Ajusta instalador do guake

--- scripts/installers/guake.sh
+++ scripts/installers/guake.sh
@@ -6,6 +6,7 @@ echo "#### Installing Guake ####"
 BASEDIR=$(dirname "$0")
 
 if ! which guake >/dev/null; then
-  apt-get install --yes "guake"
+  sudo apt-get install --yes "guake"
 fi
+
 guake --restore-preferences $BASEDIR/../others/guake.conf
