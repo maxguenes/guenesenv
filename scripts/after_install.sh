ae0c5c4152cac5188774a1d023ec24ec68adbb08
Max Guenes <maxguenes@gmail.com>
2025-07-31 22:03:08 -0300

    Executa stow + after install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Executing after install"
+
+# Forces nvm and pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 git config --global user.name "Max Guenes"
 git config --global user.email "maxguenes@gmail.com"
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,8 @@ DISTRO="$(lsb_release -d | awk -F"\t" '{print $2}')"
 echo "Using distro: $DISTRO"
 
 if [[ $DISTRO =~ "Ubuntu" ]]; then
-  echo "Installing Ubuntu packages"
+  echo "Requesting superuser permission"
+  sudo echo "Installing Ubuntu packages"
 
   apt_get_packages=(
     btop
@@ -24,15 +25,23 @@ if [[ $DISTRO =~ "Ubuntu" ]]; then
   apt_join_packages=$(printf " %s" "${apt_get_packages[@]}")
   sudo apt-get update
   sudo apt-get install --yes $apt_join_packages
-  # pip install --user spotify-cli-linux awscli awscli-local
 
   for i in $(ls "$BASEDIR/installers" | grep ".sh"); do
     echo "Running $i"
     bash "$BASEDIR/installers/$i"
   done
 
-  echo "Done installing"
+  echo "Done installing, excuting stow"
 
+  for i in $(ls "$BASEDIR/../dotfiles"); do
+    echo "Stow $i"
+    bash "$BASEDIR/stow.sh" $i
+  done
+
+  echo "Done stowing, excuting after install"
+  zsh --login -c "$BASEDIR/after_install.sh"
+
+  echo "Finished installation"
 else
   echo "No scripts prepared for $DISTRO"
   exit 73
